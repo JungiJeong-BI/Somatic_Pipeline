5851e3a61a4cc981caa8cb0cdae71076e85b8bd5
JungiJeong-BI <jungi.jeong.contact@gmail.com>
2021-12-15 15:46:36 +0900

    Reference path and Deduplication error fix

--- SomaticCaller_jjg.py
+++ SomaticCaller_jjg.py
@@ -14,18 +14,18 @@ Get_Options.add_argument("-t","--thread", required = True)
 Get_Options.add_argument("-o","--output", required = True)
 
 Options = Get_Options.parse_args()
-read1 = Options.read1; read2 = Options.read2; p_read1=Options.p_read1; p_read2=Options.p_read2; thread=Options.thread; output=Options.output
+read1 = Options.read1; read2 = Options.read2; p_read1=Options.p_read1; p_read2=Options.p_read2; thread=str(Options.thread); output=Options.output
 
 maxcpu = 40
 
-outputlog_path = "/BiO2/users/jjg/tools/Somatic_Pipeline/exp/out/log.log"
-out = "/BiO2/users/jjg/tools/Somatic_Pipeline/exp/out/"
-tmp = "/BiO2/users/jjg/tools/Somatic_Pipeline/temp/"
+outputlog_path = "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/fastq/out/temp/log.log"
+out = "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/fastq/out/"
+tmp = "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/fastq/out/temp/"
 
 print(read1)
 print(read2)
 
-reference_genome =  "/BiO2/users/jjg/DB/hg38.fa"
+reference_genome =  "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/gatkgoogle/Homo_sapiens_assembly38.fasta"
 dbsnp = "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/gatkgoogle/Homo_sapiens_assembly38.dbsnp138.vcf"
 mills = "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/gatkgoogle/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
 g1000phase1_snp = "/BiO2/users/jjg/tools/Somatic_Pipeline_DB/gatkgoogle/1000G_phase1.snps.high_confidence.hg38.vcf.gz"
@@ -65,31 +65,26 @@ def preprocessing(samplename, outputfolder):
     bwamapping = " ".join(["bwa mem -t", thread, reference_genome, 
         "".join([outputfolder,"/",samplename,"_R1.trimmed.paired.fastq.gz"]), "".join([outputfolder,"/",samplename,"_R2.trimmed.paired.fastq.gz"]),
         "| samtools view -Sb - > ", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.bam"])])
-    
     addbamrg = " ".join(["gatk AddOrReplaceReadGroups -I", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.bam"]),
     "-O", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.bam"]),
         "-LB", library_name, "-PL", platform, "-PU", platform_barcode, "-SM ", samplename,
         "--TMP_DIR", tmp])
-
     sortbam = " ".join(["samtools sort -@", thread, "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.bam"]), "-o", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.bam"])])
-
     dedup = " ".join(["gatk MarkDuplicatesSpark -I", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.bam"]),
         "-O", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.bam"]),
         "-M", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.metrics"]),
-        "--tmp-dir", tmp, "--spark-master local[{", thread, "}]"])
-
+        "--tmp-dir", tmp, "--spark-master local[" + thread + "]"])
     recal = " ".join(["gatk BaseRecalibrator -I", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.bam"]),
         "--known-sites", dbsnp, "--known-sites", mills, "--known-sites", g1000phase1_snp, "--known-sites", g1000phase1_indel,
         "--known-sites", g1000phase3, "-R", reference_genome,
         "-O", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.recal.table"])])
-
     applyrecal = " ".join(["gatk ApplyBQSR -I", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.bam"]),
         "-R", reference_genome, "--bqsr-recal-file", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.recal.table"]),
         "-O", "".join([outputfolder,"/",samplename,"_aligned.trimmomatic.bwa.rg.sorted.dedup.recal.bam"])])
     return [bwamapping, addbamrg, sortbam, dedup, recal, applyrecal]
 
 print(p_read1)
-if p_read1==None and p_read2==None:
+if p_read1!=None and p_read2!=None:
     sample = getsamplename(read1)
     #trimming(read1, read2, sample , output)
     for i_cmd in preprocessing(sample, output):
